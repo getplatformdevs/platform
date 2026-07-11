840192273b658bed855addfb95e7b4e89ad8a814
getplatformdevs <getplatform.devs@gmail.com>
2026-07-11 21:41:46 +0400

    Add files via upload

--- vendor/platform-auth.js
+++ vendor/platform-auth.js
@@ -138,6 +138,17 @@
     return callFn('check-username', { username: username }, { timeoutMs: 8000 });
   }
 
+  function changeUsername(newUsername) {
+    return callFn('account-username', { newUsername: newUsername }).then(function (data) {
+      var session = getSession();
+      if (session) {
+        session.username = data.username;
+        setSession(session);
+      }
+      return data;
+    });
+  }
+
   var USER_SVG = '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="8" r="3.6"></circle><path d="M4.5 20c1.2-4.2 4-6.3 7.5-6.3s6.3 2.1 7.5 6.3"></path></svg>';
   var GEAR_SVG = '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06A1.65 1.65 0 0 0 9 4.6a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>';
 
@@ -443,6 +454,7 @@
     login: login,
     logout: logout,
     checkUsernameAvailable: checkUsernameAvailable,
+    changeUsername: changeUsername,
     renderHeader: renderHeader,
     enforceFirstTimeGate: enforceFirstTimeGate,
     processImage: processImage
